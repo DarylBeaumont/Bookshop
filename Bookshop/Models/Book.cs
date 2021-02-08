7bcf34d2fee64fc3c16209ebec1f45ec0f564cbb
DarylBeaumont <60305113+DarylBeaumont@users.noreply.github.com>
2021-02-08 15:45:39 +0000

    Added book model.

---
+++ Bookshop/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookshop.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 1)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 1)]
+        public string Author { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Date Published")]
+        [Required]
+        public DateTime PublishDate { get; set; }
+    }
+}
